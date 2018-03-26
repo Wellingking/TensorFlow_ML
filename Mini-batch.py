1e49e1d217caf729a515ecb7d13fa369c149d5b0
Kris <Wellingking@users.noreply.github.com>
2018-03-26 17:26:31 +0800

    Update Mini-batch.py

--- Mini-batch.py
+++ Mini-batch.py
@@ -1,7 +1,29 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
 import numpy as np
-from helper import batches
+#from helper import batches
+
+#batch
+import math
+def batches(batch_size, features, labels):
+    """
+    Create batches of features and labels
+    :param batch_size: The batch size
+    :param features: List of features
+    :param labels: List of labels
+    :return: Batches of (Features, Labels)
+    """
+    assert len(features) == len(labels)
+    outout_batches = []
+    
+    sample_size = len(features)
+    for start_i in range(0, sample_size, batch_size):
+        end_i = start_i + batch_size
+        batch = [features[start_i:end_i], labels[start_i:end_i]]
+        outout_batches.append(batch)
+        
+    return outout_batches
+#batch/
 
 learning_rate = 0.001
 n_input = 784  # MNIST data input (img shape: 28*28)
